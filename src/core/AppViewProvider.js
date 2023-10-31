6f2f2bea526db126305f23b57fdfccfb75975640
剧终 <13824303245@163.com>
2023-10-31 18:19:34 +0800

    1. 新增云备份

--- src/core/AppViewProvider.js
+++ src/core/AppViewProvider.js
@@ -1,48 +1,48 @@
-const {webContents, BrowserWindow} = require("electron");
-const {EventEmitter} = require("events");
+const { webContents, BrowserWindow } = require("electron")
+const { EventEmitter } = require("events")
 
-const AppRPC = require("./AppRPC");
+const AppRPC = require("./AppRPC")
 
 const WINDOW_TYPE = {
-    MAIN_WINDOW: "mainWindow",
-    SUB_WINDOW: "subWindow"
-};
+	MAIN_WINDOW: "mainWindow",
+	SUB_WINDOW: "subWindow"
+}
 
 /**
  * @type {AppRPC.Channel}
  */
-let windowProviderChannel = null;
+let windowProviderChannel = null
 
-let lastWindowProviderRequestId = 0;
-let requestWaiters = {};
+let lastWindowProviderRequestId = 0
+let requestWaiters = {}
 
 /** FIXME: 这段代码与AppRPC中有一段代码一样，以后可以考虑合并 */
 class RequestWaiter {
-    resolve = () => {};
-    reject = () => {};
-    promise = null;
-    constructor() {
-        this.promise = new Promise((resolve, reject) => {
-            this.resolve = (value) => {
-                resolve(value);
-            }
-            this.reject = (e) => {
-                reject(e);
-            }
-        });
-    }
+	resolve = () => {}
+	reject = () => {}
+	promise = null
+	constructor() {
+		this.promise = new Promise((resolve, reject) => {
+			this.resolve = (value) => {
+				resolve(value)
+			}
+			this.reject = (e) => {
+				reject(e)
+			}
+		})
+	}
 
-    wait() {
-        return this.promise;
-    }
+	wait() {
+		return this.promise
+	}
 }
 
 function getLastWindowProviderRequestId() {
-    let id = lastWindowProviderRequestId++;
-    if (lastWindowProviderRequestId === Number.MAX_SAFE_INTEGER) {
-        lastWindowProviderRequestId = 0;
-    }
-    return id;
+	let id = lastWindowProviderRequestId++
+	if (lastWindowProviderRequestId === Number.MAX_SAFE_INTEGER) {
+		lastWindowProviderRequestId = 0
+	}
+	return id
 }
 
 /**
@@ -53,132 +53,132 @@ function getLastWindowProviderRequestId() {
  * @param {Number} viewInfo.webContentId webContentId
  */
 function onWindowProviderResponse(viewInfo) {
-    let waiter = requestWaiters[viewInfo.reqId];
-    if (!waiter) {
-        console.error(new Error("invalid view info"));
-        return;
-    }
-    waiter.resolve(viewInfo);
+	let waiter = requestWaiters[viewInfo.reqId]
+	if (!waiter) {
+		console.error(new Error("invalid view info"))
+		return
+	}
+	waiter.resolve(viewInfo)
 
-    delete requestWaiters[viewInfo.reqId];
+	delete requestWaiters[viewInfo.reqId]
 }
 
+class ShellAppView extends EventEmitter {
+	/** @type {webContents} */
+	webContents = null
+	constructor(wc) {
+		super()
+		this.webContents = wc
+	}
 
-class ShellAppView extends EventEmitter{
-    /** @type {webContents} */
-    webContents = null;
-    constructor(wc) {
-        super();
-        this.webContents = wc;
-    }
-
-    loadURL(url) {
-        return this.webContents.loadURL(url);
-    }
+	loadURL(url) {
+		return this.webContents.loadURL(url)
+	}
 }
 
 const subViewManager = {
-    lastViewId: 0,
-    views: {},
-    getLastViewId() {
-        // 不同于RPC类似的Request，理论上lastViewId用到最大的时候会变为0然后继续，但是这个为0的view很有可能依然存在
-        // 不过Number.MAX_SAFE_INTEGER，应该在有生之年是用不完的了
-        let id = this.lastViewId++;
-        if (this.lastViewId >= Number.MAX_SAFE_INTEGER) {
-            this.lastViewId = 0;
-        }
-        return id;
-    },
+	lastViewId: 0,
+	views: {},
+	getLastViewId() {
+		// 不同于RPC类似的Request，理论上lastViewId用到最大的时候会变为0然后继续，但是这个为0的view很有可能依然存在
+		// 不过Number.MAX_SAFE_INTEGER，应该在有生之年是用不完的了
+		let id = this.lastViewId++
+		if (this.lastViewId >= Number.MAX_SAFE_INTEGER) {
+			this.lastViewId = 0
+		}
+		return id
+	},
 
-    async callViewProvider(webContentId=null, method="", args=[]) {
-        let reqId = getLastWindowProviderRequestId();
-        windowProviderChannel.send({
-            reqId,
-            webContentId,
-            method,
-            args
-        });
-        let waiter = new RequestWaiter();
-        requestWaiters[reqId] = waiter;
-        let response = await waiter.wait();
-        return response
-    },
+	async callViewProvider(webContentId = null, method = "", args = []) {
+		let reqId = getLastWindowProviderRequestId()
+		windowProviderChannel.send({
+			reqId,
+			webContentId,
+			method,
+			args
+		})
+		let waiter = new RequestWaiter()
+		requestWaiters[reqId] = waiter
+		let response = await waiter.wait()
+		return response
+	},
 
-    async createView() {
-        let {webContentId} = this.callViewProvider();
-        const _this = this;
-        let viewProxy = new Proxy({}, {
-            get(target, p, receiver) {
-                return async function(...args) {
-                    return await _this.callViewProvider(webContentId, p, args)
-                }
-            },
-            set(target, p, value, receiver) {
-                // TODO:
-            }
-        })
+	async createView() {
+		let { webContentId } = this.callViewProvider()
+		const _this = this
+		let viewProxy = new Proxy(
+			{},
+			{
+				get(target, p, receiver) {
+					return async function (...args) {
+						return await _this.callViewProvider(webContentId, p, args)
+					}
+				},
+				set(target, p, value, receiver) {
+					// TODO:
+				}
+			}
+		)
 
-        return viewProxy
-    }
+		return viewProxy
+	}
 }
 
-function get_nxshell_logo() {
-
-}
-
+function get_nxshell_logo() {}
 
 const windowProviders = {
-    async mainWindow (flags) {
-        /** @type {import("electron/main").BrowserWindowConstructorOptions} */
-        let options = {
-            width: 1250,
-            minWidth: 1250,
-            height: 720,
-            minHeight: 720,
-            show: false,
-            webPreferences: {
-                preload: `${__dirname}/AppClient.js`,
-                webviewTag: true,
-                contextIsolation: false,
-                enableRemoteModule: true
-            },
-            icon: get_nxshell_logo()
-        };
-        let transparent = false;
-        for (let winFlag of (flags || [])) {
-            if (winFlag === "frameless") {
-                options.frame = false;
-            } else if (winFlag === "hidden") {
-                options.titleBarStyle = "hidden";
-            } else if (winFlag === "transparent") {
-                options.transparent = true;
-                transparent = true;
-            }
-        }
+	async mainWindow(flags) {
+		/** @type {import("electron/main").BrowserWindowConstructorOptions} */
+		let options = {
+			width: 1250,
+			minWidth: 1250,
+			height: 720,
+			minHeight: 720,
+			show: false,
+			webPreferences: {
+				preload: `${__dirname}/AppClient.js`,
+				sandbox: false,
+				webviewTag: true,
+				contextIsolation: false,
+				webSecurity: false, // 禁用同源策略
+				enableRemoteModule: true
+			},
+			icon: get_nxshell_logo()
+		}
+		let transparent = false
+		for (let winFlag of flags || []) {
+			if (winFlag === "frameless") {
+				options.frame = false
+			} else if (winFlag === "hidden") {
+				options.titleBarStyle = "hidden"
+			} else if (winFlag === "transparent") {
+				options.transparent = true
+				transparent = true
+			}
+		}
 
-        let window = new BrowserWindow(options);
-        if (transparent) {
-            // window.setIgnoreMouseEvents(true);
-        }
+		let window = new BrowserWindow(options)
+		if (transparent) {
+			// window.setIgnoreMouseEvents(true);
+		}
 
-        window.once("ready-to-show", () => {
-            window.show();
-        })
+		window.once("ready-to-show", () => {
+			window.show()
+		})
 
-        return window;
-    },
+		return window
+	},
 
-    async subWindow (flags) {
-        // 总是创建新窗口
-        return await windowProviders.mainWindow(flags);
-    }
+	async subWindow(flags) {
+		// 总是创建新窗口
+		return await windowProviders.mainWindow(flags)
+	}
 }
 
-
 async function createWindow(windowType, flags) {
-    let windowCtor = windowProviders[windowType] || windowProviders.mainWindow;
+	let windowCtor = windowProviders[windowType] || windowProviders.mainWindow
 
-    return await windowCtor(flags);
+	return await windowCtor(flags)
 }
 
 /**
@@ -187,20 +187,20 @@ async function createWindow(windowType, flags) {
  * @param {Channel} winProviderChannel window管理提供者提供的channel
  */
 function registerWindowProvider(winProviderChannel) {
-    if (windowProviderChannel) {
-        return;
-    }
-    windowProviderChannel = winProviderChannel;
+	if (windowProviderChannel) {
+		return
+	}
+	windowProviderChannel = winProviderChannel
 
-    winProviderChannel.on("data", (data) => {
-        onWindowProviderResponse(data);
-    });
+	winProviderChannel.on("data", (data) => {
+		onWindowProviderResponse(data)
+	})
 }
 
 module.exports = {
-    WINDOW_TYPE,
+	WINDOW_TYPE,
 
-    registerWindowProvider,
+	registerWindowProvider,
 
-    createWindow
-};
+	createWindow
+}
